c5d4ec435090a97924356cbd3232e07d1447beb3
rmabry1 <156986963+rmabry1@users.noreply.github.com>
2024-04-26 20:43:30 -0500

    Update main_menu.gd

--- project/source/main_menu.gd
+++ project/source/main_menu.gd
@@ -2,8 +2,8 @@ extends Node
 
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file('res://node.tscn')
+	get_tree().change_scene_to_file("res://source/node.tscn")
 
 
 func _on_level_2_pressed():
-	get_tree().change_scene_to_file("res://level_2.tscn")
+	get_tree().change_scene_to_file("res://source/level_2.tscn")
